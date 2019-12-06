4ad9e6b989f537721ae61bd9ee8e56e6faabff62
Craig Morris <cmorris98@me.com>
2019-12-06 07:35:01 -0700

    First working version for testing

--- driver.lua
+++ driver.lua
@@ -7,30 +7,25 @@
 -- end
 
 do	--Globals
-	TEST_PROPERTY = TEST_PROPERTY or 'Hello World 2'
-
 	Fountain = {}
+	Timer = {}
 	FOUNTAIN_PROXY = 5000
-	PULSE = 500
+	PULSE_ON = PULSE_ON or 500
+	PULSE_OFF = PULSE_OFF or 500
+	HOLD_OR_PULSE_ON_RELAY = HOLD_OR_PULSE_ON_RELAY or 'Pulse'
+	HOLD_OR_PULSE_OFF_RELAY = HOLD_OR_PULSE_OFF_RELAY or 'Pulse'
 	ON_RELAY = 1
 	OFF_RELAY = 2
 	ON_CONTACT = 3
 	OFF_CONTACT = 4
-	DEBUGPRINT = true
+	DEBUG_PRINT = DEBUG_PRINT or false
 end
 
 function dbg (strDebugText, ...)
-	if (DEBUGPRINT) then 
+	if (DEBUG_PRINT) then 
 		print (os.date ('%x %X : ') .. (strDebugText or ''), ...) 
 	end
 end
-
--- function dbgdump (strDebugText, ...)
--- 	if (DEBUGPRINT) then 
--- 		hexdump (strDebugText or '') 
--- 		print (...) 
--- 	end
--- end
 
 function OnDriverDestroyed ()
 	dbg('OnDriverDestroyed called')
@@ -47,13 +42,26 @@ function OnPropertyChanged (strProperty)
 		return
 	end
 
-	if (strProperty == 'TestProperty') then
+	if (strProperty == 'Debug Print') then
 		if (value == 'Off') then
-			DEBUGPRINT = false
+			dbg('Turning off debug print')
+			DEBUG_PRINT = false
 		elseif (value == 'On') then
-			DEBUGPRINT = true
+			DEBUG_PRINT = true
+			dbg('Turning on debug print')
 		end
-		dbg('TestProperty set to ' .. value)
+	elseif (strProperty == 'Pulse Time On (milliseconds)') then
+		PULSE_ON = tonumber(value)
+		dbg('PULSE_ON set to ' .. PULSE_ON)
+	elseif (strProperty == 'Pulse Time Off (milliseconds)') then
+		PULSE_OFF = tonumber(value)
+		dbg('PULSE_OFF set to ' .. PULSE_OFF)
+	elseif (strProperty == 'Hold or Pulse On') then
+		HOLD_OR_PULSE_ON_RELAY = value
+		dbg('HOLD_OR_PULSE_ON_RELAY set to ' .. HOLD_OR_PULSE_ON_RELAY)
+	elseif (strProperty == 'Hold or Pulse Off') then
+		HOLD_OR_PULSE_OFF_RELAY = value
+		dbg('HOLD_OR_PULSE_OFF_RELAY set to ' .. HOLD_OR_PULSE_OFF_RELAY)
 	end
 end
 
@@ -61,13 +69,6 @@ end
 -- -- 	C4:UpdateProperty ('Driver Information', info)
 -- -- 	print (os.date ('%x %X : ') .. info)
 -- -- end
-
--- -- When the timer expires, open both relays
--- function OnTimerExpired (idTimer)
--- 	dbg('timer ' .. idTimer .. 'expired')
--- 	C4:SendToProxy (ON_RELAY, "OPEN", '')
--- 	C4:SendToProxy (OFF_RELAY, "OPEN", '')
--- end
 
 function ReceivedFromProxy (idBinding, strCommand, tParams)
 	dbg (idBinding .. ' command: ' .. strCommand)
@@ -82,19 +83,19 @@ function ReceivedFromProxy (idBinding, strCommand, tParams)
 	end
 
 	if (idBinding == ON_CONTACT) then
-		if (strCommand == "CLOSED") then
+		if (strCommand == "CLOSED" or strCommand == "STATE_ClOSED") then
 			dbg('Foutain is ON')
 			C4:SendToProxy (FOUNTAIN_PROXY, 'UP', {LEVEL = 1}, "NOTIFY", true)
-		elseif (strCommand == "OPENED") then
+		elseif (strCommand == "OPENED" or strCommand == "STATE_OPENED") then
 			dbg('Fountain is turning OFF')
 		end
 	end
 
 	if (idBinding == OFF_CONTACT) then
-		if (strCommand == "CLOSED") then
+		if (strCommand == "CLOSED" or strCommand == "STATE_ClOSED") then
 			dbg('Foutain is OFF')
 			C4:SendToProxy (FOUNTAIN_PROXY, 'DOWN', {LEVEL = 1}, "NOTIFY", true)
-		elseif (strCommand == "OPENED") then
+		elseif (strCommand == "OPENED" or strCommand == "STATE_OPENED") then
 			dbg('Fountain is turning ON')
 		end
 	end
@@ -110,12 +111,27 @@ function Fountain.OnCommand ()
 	dbg ('Turning on fountain')
 	C4:SendToProxy (ON_RELAY, "CLOSE", '')
 	C4:SendToProxy (OFF_RELAY, "OPEN", '')
-	-- C4:AddTimer (PULSE, 'MILLISECONDS', false)
+	if (HOLD_OR_PULSE_ON_RELAY == 'Pulse') then
+		Timer.PulseOn = C4:AddTimer (PULSE_ON, 'MILLISECONDS', false)
+	end
 end
 
 function Fountain.OffCommand ()
 	dbg ('Turning off fountain')
 	C4:SendToProxy (ON_RELAY, "OPEN", '')
 	C4:SendToProxy (OFF_RELAY, "CLOSE", '')
-	-- C4:AddTimer (PULSE, 'MILLISECONDS', false)
+	if (HOLD_OR_PULSE_OFF_RELAY == 'Pulse') then
+		Timer.PulseOff = C4:AddTimer (PULSE_OFF, 'MILLISECONDS', false)
+	end
+end
+
+-- When the timer expires, open appropriate relay
+function OnTimerExpired (idTimer)
+	dbg('timer ' .. idTimer .. ' expired')
+
+	if (idTimer == Timer.PulseOn) then
+		C4:SendToProxy (ON_RELAY, "OPEN", '')
+	elseif (idTimer == Timer.PulseOff) then
+		C4:SendToProxy (OFF_RELAY, "OPEN", '')
+	end
 end
